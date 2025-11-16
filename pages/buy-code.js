454f63a5c11df9724a378eb9ba3ed44d8b171466
Tasiu Ismail <mynameistasiu@gmail.com>
2025-11-16 10:47:26 +0100

    Update CODE_PRICE from 7000 to 15000

--- pages/buy-code.js
+++ pages/buy-code.js
@@ -6,7 +6,7 @@ import { saveTx } from '../utils/storage';
 import { loadTx } from "../utils/storage";
 
 
-const CODE_PRICE = 7000;
+const CODE_PRICE = 15000;
 const WA = '+2347078323440';
 
 export default function BuyCode() {
